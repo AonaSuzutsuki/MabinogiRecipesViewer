4d0a36ae0b2988bdfb6a46078b41b671b6d03e61
AonaSuzutsuki <rvvfake.org@gmail.com>
2023-01-13 18:00:52 +0900

    added test tree view.

--- CoonInformationViewer/Models/Searchers/SearchWindowModel.cs
+++ CoonInformationViewer/Models/Searchers/SearchWindowModel.cs
@@ -1,8 +1,46 @@
-﻿using CommonStyleLib.Models;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CommonStyleLib.Models;
+using Reactive.Bindings;
 
 namespace CookInformationViewer.Models.Searchers
 {
+    public class SearchNode
+    {
+        public SearchNode? Parent { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public List<SearchNode>? Children { get; set; }
+    }
+
     public class SearchWindowModel : ModelBase
     {
+        private ObservableCollection<SearchNode> _searchNodes = new();
+
+        public ObservableCollection<SearchNode> SearchItems
+        {
+            get => _searchNodes;
+            set => SetProperty(ref _searchNodes, value);
+        }
+
+        public SearchWindowModel()
+        {
+            SearchItems.Add(new SearchNode
+            {
+                Name = "1",
+                Children = new List<SearchNode>
+                {
+                    new SearchNode
+                    {
+                        Name = "2"
+                    },
+                    new SearchNode
+                    {
+                        Name = "3"
+                    }
+                }
+            });
+        }
     }
 }
--- CoonInformationViewer/ViewModels/Searchers/SearchWindowViewModel.cs
+++ CoonInformationViewer/ViewModels/Searchers/SearchWindowViewModel.cs
@@ -8,13 +8,18 @@ using CommonStyleLib.ViewModels;
 using CommonStyleLib.Views;
 using CookInformationViewer.Models.Searchers;
 using CookInformationViewer.Views.Searches;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 
 namespace CookInformationViewer.ViewModels.Searchers
 {
     public class SearchWindowViewModel : ViewModelWindowStyleBase
     {
+        public ReadOnlyReactiveCollection<SearchNode> SearchItems { get; set; }
+
         public SearchWindowViewModel(IWindowService windowService, SearchWindowModel model) : base(windowService, model)
         {
+            SearchItems = model.SearchItems.ToReadOnlyReactiveCollection().AddTo(CompositeDisposable);
         }
     }
 }
